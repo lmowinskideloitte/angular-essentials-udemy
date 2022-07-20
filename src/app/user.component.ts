4f3241380691ec4903203129812cbd091eecaed0
Lukasz Mowinski <lmowinski@deloittece.com>
2022-07-20 22:06:21 +0200

    input form, turbo basic crude extreme edition v0.01.1 component

---
+++ src/app/user.component.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <input type="text" (input)="onUserInput($event)" value="{{ name }}">
+    <p>mmmm, aha [{{ name }}]</p>
+    <p>I'm the user component B)))</p>
+  `
+})
+export class UserComponent {
+  name = 'Młody Gypsy';
+
+  onUserInput(event: any) {
+    this.name = event.target.value;
+  }
+}
