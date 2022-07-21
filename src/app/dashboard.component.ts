e1eb93a5165c0cb12319a205eee0fe003c7787db
Lukasz Mowinski <lmowinski@deloittece.com>
2022-07-21 15:13:44 +0200

    bootstrap

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -6,8 +6,13 @@ import { Component } from "@angular/core";
 })
 export class DashboardComponent {
   loadState = 'loading';
+  number = 0;
 
   onSwitchState() {
     this.loadState = 'finished';
+  }
+
+  onIncrement() {
+    this.number += 1;
   }
 }
